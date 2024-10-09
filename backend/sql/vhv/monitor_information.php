b959e11957086aa626f12448eab66a906a7fe2ac
Lalita123456 <6431503051@lamduan.mfu.ac.th>
2024-10-09 15:08:47 +0700

    update code

--- backend/sql/vhv/monitor_information.php
+++ backend/sql/vhv/monitor_information.php
@@ -4,9 +4,20 @@ include_once '../db_connection.php';
 
 try {
     // สร้างคำสั่ง SQL สำหรับดึงข้อมูลจากตาราง patient_information และ patient_address
-    $sql = "SELECT p.full_name, a.province, a.amphur, a.tambon, a.soi, a.moo , a.number
-            FROM patient_information p
-            JOIN patient_address a ON p.id_patient_address = a.id";
+    $sql = "SELECT p.full_name, 
+               a.province, 
+               a.amphur, 
+               a.tambon, 
+               a.soi, 
+               a.moo, 
+               a.number, 
+               m.monitor_date, 
+               m.monitor_status, 
+               m.last_update 
+        FROM patient_information p
+        JOIN patient_address a ON p.id_patient_address = a.id
+        JOIN monitor_information m ON p.id_patient = m.patient_id"; // ใช้ id_patient เพื่อเชื่อมต่อกับ monitor_information
+
 
     // เตรียมและรันคำสั่ง SQL
     $stmt = $conn->prepare($sql);
